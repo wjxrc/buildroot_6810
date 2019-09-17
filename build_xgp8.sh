4b2f18c89c7bec1aec147df44c319be4b55cfd0a
wangjianxin <wangjianxin02@raisecom.com>
2019-09-17 18:43:08 +0800

    xgp8 build 脚本

--- build_xgp8.sh
+++ build_xgp8.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 make clean
-make 6810_mvebu_armv8_le_defconfig
+make xgp8_be_defconfig
 make
-cp .config configs/6810_mvebu_armv8_le_defconfig 
-cp output/build/busybox-1.24.1/.config package/busybox/busybox.config 
+cp .config configs/xgp8_be_defconfig
+cp output/build/busybox-1.24.1/.config package/busybox/busybox_xgp8.config 
