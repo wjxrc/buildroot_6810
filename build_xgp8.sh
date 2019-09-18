4a617062f8c7b5887a560b9c88f800c7c6fdbef3
wangjianxin <wangjianxin02@raisecom.com>
2019-09-18 11:27:44 +0800

    xgp8 build script

--- build_xgp8.sh
+++ build_xgp8.sh
@@ -4,3 +4,11 @@ make xgp8_be_defconfig
 make
 cp .config configs/xgp8_be_defconfig
 cp output/build/busybox-1.24.1/.config package/busybox/busybox_xgp8.config 
+cd output/images/
+rm -rf rootfs
+mkdir rootfs
+tar -xf rootfs.tar -C rootfs/
+rm -rf rootfs.tar
+tar -cf rootfs.tar rootfs
+cd ../../
+cp output/images/rootfs.tar .
