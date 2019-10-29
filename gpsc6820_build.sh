3f38708e2d69794260ffae95811c6a5044cfe2b2
wangjianxin <wangjianxin02@raisecom.com>
2019-10-29 11:38:41 +0800

    gdb,memstat,procps

--- gpsc6820_build.sh
+++ gpsc6820_build.sh
@@ -1,16 +1,45 @@
 #/bin/bash
 #rm -rf output
 #make 6820gpsc_tool_alone_defconfig
+dc="6820gpsc_tool_alone_defconfig"
+bc="package/busybox/busybox_gpsc.config"
+function help()
+{
+		set +x
+		echo "you input is $*"
+		echo "you input is $@"
+		echo "usage is:"
+		echo "$0 1---only make"
+		echo "$0 2---menuconfig"
+		echo "$0 3---busybox menuconfig"
+		echo "$0 4---rebuild all"
+		echo "$0 1---only make"
+		echo "$0 1---only make"
+}
 set -x
 if [ $# -lt 1 ] ; then
-	make
-elif [ $1 = menuconfig ]; then
+	help $*
+	exit 0
+elif [ $1 = 1 -o $1 = "make" ]; then
+	make 
+elif [ $1 = 2 -o $1 = "menuconfig" ]; then
 	make menuconfig
-elif [ $1 = "a" ]; then 
+	exit 0
+elif [ $1 = 3 ]; then
+	make busybox-menuconfig
+	exit 0
+elif [ $1 = "a" -o $1 = 4 ]; then 
 	rm -rf output
-	make 6820gpsc_tool_alone_defconfig
+	make  $dc
 	make
 fi
 
-cp .config configs/6820gpsc_tool_alone_defconfig
-cp output/build/busybox-1.24.1/.config  package/busybox/busybox_gpsc.config
+cp .config configs/${dc}
+cp output/build/busybox-1.24.1/.config  $bc
+
+cd output 
+rm -rf rootfs*
+cp target rootfs -r
+mkdir -p home/root
+tar -jcf rootfs.tar.bz2 rootfs
+cp rootfs.tar.bz2 ../
