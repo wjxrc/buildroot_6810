99e343a43667d653d1ef906732328d96c94f2152
YanLong Shi <shiyanlong@raisecom.com>
2019-11-20 15:44:19 +0800

    优化编译方式

--- build_6810_xep8.sh
+++ build_6810_xep8.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 #make clean
-rm -rf output
+rm -rf  output
 make 6810_mvebu_armv8_le_defconfig
 make
-cp .config configs/6810_mvebu_armv8_le_defconfig 
-cp output/build/busybox-1.24.1/.config package/busybox/busybox.config 
+cp .config configs/6810_mvebu_armv8_le_defconfig
+cp output/build/busybox-1.24.1/.config package/busybox/busybox_xep8.config 
+
